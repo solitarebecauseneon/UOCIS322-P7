f2facacd9a0a8013ae437d71f90bb06b9d132052
alih <alihassanijr1998@gmail.com>
2021-06-04 09:44:53 -0700

    Minor fixes

--- DockerAuth/password.py
+++ DockerAuth/password.py
@@ -1,10 +1,13 @@
 from passlib.apps import custom_app_context as pwd_context
 
+
 def hash_password(password):
     return pwd_context.encrypt(password)
 
+
 def verify_password(password, hashVal):
     return pwd_context.verify(password, hashVal)
+
 
 if __name__ == "__main__":
     testPass = "UOCIS322"
--- DockerAuth/testToken.py
+++ DockerAuth/testToken.py
@@ -3,28 +3,29 @@ from itsdangerous import (TimedJSONWebSignatureSerializer \
                                   SignatureExpired)
 import time
 
-# initialization
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+SECRET_KEY = 'test1234@#$'
+
 
 def generate_auth_token(expiration=600):
-   # s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-   s = Serializer('test1234@#$', expires_in=expiration)
-   # pass index of user
-   return s.dumps({'id': 1})
+   s = Serializer(SECRET_KEY, expires_in=expiration)
+   return s.dumps({'id': 5, 'name': 'Ryan'})
+
 
 def verify_auth_token(token):
-    s = Serializer('test1234@#$')
+    s = Serializer(SECRET_KEY)
     try:
         data = s.loads(token)
     except SignatureExpired:
-        return None    # valid token, but expired
+        return "Expired token!"    # valid token, but expired
     except BadSignature:
-        return None    # invalid token
-    return "Success"
+        return "Invalid token!"    # invalid token
+    return f"Success! Welcome {data['username']}."
+
 
 if __name__ == "__main__":
     t = generate_auth_token(10)
     for i in range(1, 20):
-	print verify_auth_token(t)
+        print(verify_auth_token(t))
         time.sleep(1)
+
+    print(verify_auth_token("somerandomstring"))
