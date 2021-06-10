7df186c39d57b9cb9a2089b3bdd2afc75a686009
James Taylor <solitarebecauseneon@gmail.com>
2021-06-10 07:08:06 -0700

    V1U2 updated password.py

--- brevets/website/password.py
+++ brevets/website/password.py
@@ -1,21 +1,11 @@
-from passlib.apps import custom_app_context as pwd_context
+from passlib.hash import sha256_crypt as pwd_context
 
 
 def hash_password(password):
     """returns hashed password for user_db storage"""
-    return pwd_context.encrypt(password)
+    pwd_context.using(salt="wnsT7Yr92oJoP28r").encrypt(password)
 
 
 def verify_password(password, hashVal):
     """returns true if password hash present in user_db, false otherwise"""
     return pwd_context.verify(password, hashVal)
-
-
-if __name__ == "__main__":
-    testPass = "UOCIS322"
-    hVal = hash_password(testPass)
-    print(hVal)
-    if verify_password(testPass, hVal):
-        print ("Success")
-    else:
-        print ("Failure")
