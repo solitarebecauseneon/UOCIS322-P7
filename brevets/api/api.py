15f3d457dd1cb2dcb2c23c42492d159ffbba4304
James Taylor <solitarebecauseneon@gmail.com>
2021-06-10 12:31:12 -0700

    V1U27 updated api.py

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -172,7 +172,7 @@ class TokenGeneration(Resource):
             if user_info['username'] == str(username) and user_info['password'] == str(password):
                 expiration = 600
                 s = generate_auth_token(SECRET_KEY, expiration)
-                return s.dumps({'username': username})
+                return str(s.dumps({'username': username}))
         return "", 401
 
 
