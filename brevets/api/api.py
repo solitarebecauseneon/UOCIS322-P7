c56fe5cd3802de7c2b7b52c6d0908f7b0e1be028
James Taylor <solitarebecauseneon@gmail.com>
2021-06-10 06:08:49 -0700

    U43: minor updates to api.py

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -27,7 +27,8 @@ def retrieve_user(uid=-2, username=""):
     return user_db.timestable.find_one({'_id': int(uid)})
 
 
-class UserCheck(Resource):
+@app.route('/user_check')
+def user_check(self):
     """
     returns a JSON object, {uid:int, username:str, password:str}
 
@@ -38,31 +39,30 @@ class UserCheck(Resource):
     password: returns value only if needed for User object
 
     """
-    def get(self):
-        # variables
-        uid = request.args.get('uid', default=-1)
-        username = request.args.get('username', default='-1')
-        # checking for user in user_db
-        if int(uid) == -1:  # if no uid given
-            app.logger.debug("usercheck/username: {}".format(username))
-            user_entry = retrieve_user(username=username)  # retrieve entry using username
-        else:
-            app.logger.debug("usercheck/uid: {}".format(uid))
-            user_entry = retrieve_user(uid)  # uid given: retrieve entry using uid
-        app.logger.debug("registerUser/output: {}".format(user_entry))
-        if user_entry:  # if an entry is found
-            result = {
-                'uid': user_entry['_id'],
-                'username': user_entry['username'],
-                'password': user_entry['password']
-            }
-        else:  # no entry found; return uid as -1, other values as blanks
-            result = {
-                'uid': -1,
-                'username': '',
-                'password': ''
-            }
-        return jsonify(result), 200
+    # variables
+    uid = request.args.get('uid', default=-1)
+    username = request.args.get('username', default='-1')
+    # checking for user in user_db
+    if int(uid) == -1:  # if no uid given
+        app.logger.debug("usercheck/username: {}".format(username))
+        user_entry = retrieve_user(username=username)  # retrieve entry using username
+    else:
+        app.logger.debug("usercheck/uid: {}".format(uid))
+        user_entry = retrieve_user(uid)  # uid given: retrieve entry using uid
+    app.logger.debug("registerUser/output: {}".format(user_entry))
+    if user_entry:  # if an entry is found
+        result = {
+            'uid': user_entry['_id'],
+            'username': user_entry['username'],
+            'password': user_entry['password']
+        }
+    else:  # no entry found; return uid as -1, other values as blanks
+        result = {
+            'uid': -1,
+            'username': '',
+            'password': ''
+        }
+    return jsonify(result), 200
 
 
 def retrieve(val_include="default"):
@@ -110,19 +110,19 @@ def json_form(result, top):
     return data
 
 
-class PullPassword(Resource):
+@app.route('/pass_check')
+def pull_password(self):
     """
     Retrieves hashed password for specified username
     """
-    def get(self):
-        username = request.args.get('username')
-        user = retrieve_user(username=username)
-        app.logger.debug("pullpassword/user: {}".format(user))
-        if user:
-            result = {'password': user['password']}
-        else:
-            result = {'password': None}
-        return jsonify(result)
+    username = request.args.get('username')
+    user = retrieve_user(username=username)
+    app.logger.debug("pullpassword/user: {}".format(user))
+    if user:
+        result = {'password': user['password']}
+    else:
+        result = {'password': None}
+    return jsonify(result)
 
 
 @app.route('/hidden')
@@ -154,22 +154,22 @@ def register():
     return jsonify(user), 201
 
 
-class TokenGeneration(Resource):
+@app.route('/token')
+def token_generation(self):
     """
     Checks if username and hashword (hashed password) exists in
     database. If so, generates and returns a token. Else, returns abort request
     """
-    def get(self):
-        username = request.args.get('username')
-        hashword = request.args.get('password')
-        user_info = retrieve_user(username=username)
-        if user_info:
-            if user_info['username'] == username and user_info['hashword'] == hashword:
-                expiration = 600
-                s = generate_auth_token(SECRET_KEY, expiration)
-                result = {"token": s, "duration": str(expiration)}
-                return jsonify(result=result)
-        return 401
+    username = request.args.get('username')
+    hashword = request.args.get('password')
+    user_info = retrieve_user(username=username)
+    if user_info:
+        if user_info['username'] == username and user_info['hashword'] == hashword:
+            expiration = 600
+            s = generate_auth_token(SECRET_KEY, expiration)
+            result = {"token": s, "duration": str(expiration)}
+            return jsonify(result=result)
+    return "", 401
 
 
 class ListAll(Resource):
@@ -210,9 +210,7 @@ class ListCloseOnly(Resource):
 
 # Create routes
 # Another way, without decorators
-api.add_resource(UserCheck, '/user_check')
-api.add_resource(TokenGeneration, '/token')
-api.add_resource(PullPassword, '/pass_check')
+
 api.add_resource(ListAll, '/listAll/<string:dtype>', '/listAll/')
 api.add_resource(ListOpenOnly, '/listOpenOnly/<string:dtype>', '/listOpenOnly/')
 api.add_resource(ListCloseOnly, '/listCloseOnly/<string:dtype>', '/listCloseOnly/')
