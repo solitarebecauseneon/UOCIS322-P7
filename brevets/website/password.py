249f6d3977356871bc6e4261ab374f64043057ce
James Taylor <solitarebecauseneon@gmail.com>
2021-06-10 06:04:15 -0700

    U42: minor updates to password.py

--- brevets/website/password.py
+++ brevets/website/password.py
@@ -3,7 +3,7 @@ from passlib.hash import sha256_crypt as pwd_context
 
 def hash_password(password):
     """returns hashed password for user_db storage"""
-    pwd_context.using(salt="some string").encrypt(password)
+    pwd_context.using(salt="wnsT7Yr92oJoP28r").encrypt(password)
 
 
 def verify_password(password, hashVal):
