1c2007ee8318c0842c931e11d3458275e69e35ad
James Taylor <solitarebecauseneon@gmail.com>
2021-06-10 07:27:02 -0700

    V1U5 updated password.py

--- brevets/website/password.py
+++ brevets/website/password.py
@@ -3,7 +3,7 @@ from passlib.hash import sha256_crypt as pwd_context
 
 def hash_password(password):
     """returns hashed password for user_db storage"""
-    pwd_context.using(salt="wnsT7Yr92oJoP28r").encrypt(password)
+    pwd_context.encrypt(password, salt='abcdefgh12345678')
 
 
 def verify_password(password, hashVal):
